5bc083e2033fcdf40b1a84faf7e1f7256cc85834
Omer Szerer <omerszerer@gmail.com>
2024-09-23 16:07:22 +0200

    Add 'hero description' to section 1

--- src/App.js
+++ src/App.js
@@ -13,32 +13,32 @@ export default function App() {
             <a
               href="https://wannathis.one/"
               aria-label="wannathis"
-              className="link w-inline-block"
+              className="link to wannathis website"
             >
               <img
                 src="https://d2pas86kykpvmq.cloudfront.net/landings/inflatable/ic-logo.svg"
                 loading="eager"
                 alt=""
-                className="ic-logo"
+                className="wannathis-logo"
               />
             </a>
           </div>
-          <a href="#download" className="btn-header w-inline-block">
+          <a href="#download" className="btn-header">
             <div className="btn-text desktop">
               Download for Figma and Blender
             </div>
-            <div className="btn-text mob">Download</div>
+            <div className="btn-text mobile">Download</div>
           </a>
         </div>
         <div className="hero">
           <div className="hero-img">
             <img
-              className="hero-1"
+              className="hero-1-left"
               src="https://d2pas86kykpvmq.cloudfront.net/landings/inflatable/hero-1.png"
               alt="yellow inflatable abstract 3d illustration"
             />
             <img
-              className="hero-2"
+              className="hero-2-right"
               src="https://d2pas86kykpvmq.cloudfront.net/landings/inflatable/hero-3.png"
               alt="silver inflatable abstract 3d illustration"
             />
@@ -51,17 +51,37 @@ export default function App() {
           </div>
           <div className="hero-img-2">
             <img
-              className="hero-3"
+              className="hero-3-center"
               src="https://d2pas86kykpvmq.cloudfront.net/landings/inflatable/hero-2.png"
               alt="purple inflatable abstract 3d illustration"
             />
             <img
-              className="hero-4"
+              className="hero-4-center"
               src="https://d2pas86kykpvmq.cloudfront.net/landings/inflatable/hero-4.png"
               alt="bubbles inflatable abstract 3d illustration"
             />
           </div>
         </div>
+        <div className="hero-description">
+          <div className="hero-description-wrapper">
+            <div className="hero-description-left">
+              <h4>Playful & captivating</h4>
+            </div>
+            <div className="hero-description-center">
+              <img
+                src="https://d2pas86kykpvmq.cloudfront.net/landings/inflatable/star.png"
+                alt="star"
+              />
+            </div>
+            <div className="hero-description-right">
+              <p>
+                Add sense of playfulness into your projects that's perfect for
+                capturing attention and creating memorable experiences.
+              </p>
+            </div>
+          </div>
+        </div>
+        ``
       </div>
       <div className="section-2">
         <div></div>
